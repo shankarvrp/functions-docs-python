2337ccb90d7609b8bc6e5fcbe010aff1b2c7efc4
shankarvrp <shankarvrp@hotmail.com>
2023-08-21 11:28:37 +0530

    Delete ServiceBusOutput directory

--- ServiceBusOutput/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-import azure.functions as func
-
-def main(req: func.HttpRequest, message: func.Out[str]) -> func.HttpResponse:
-
-    value = req.params.get('message')
-    message.set(value)
-    return 'Message sent'
