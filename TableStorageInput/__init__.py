e71bf6ecb0e9ec78bdc29ef94cfd8ab92f3a00e5
shankarvrp <shankarvrp@hotmail.com>
2023-08-21 11:28:52 +0530

    Delete TableStorageInput directory

--- TableStorageInput/__init__.py
+++
@@ -1,8 +0,0 @@
-import json
-
-import azure.functions as func
-
-def main(req: func.HttpRequest, messageJSON) -> func.HttpResponse:
-
-    message = json.loads(messageJSON)
-    return func.HttpResponse(f"Table row: {messageJSON}")
