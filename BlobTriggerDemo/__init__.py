a9a13d1d797b36a5f2b60811b625309722020608
shankarvrp <shankarvrp@hotmail.com>
2023-08-21 11:26:56 +0530

    Delete BlobTriggerDemo directory

--- BlobTriggerDemo/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-import azure.functions as func
-
-def main(blob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {blob.name}\n"
-                 f"Blob Size: {blob.length} bytes")
