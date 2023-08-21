125f0ab2964c758b3d6b5aa55e443c75c38c040e
shankarvrp <shankarvrp@hotmail.com>
2023-08-21 11:28:44 +0530

    Delete ServiceBusTriggerDemo directory

--- ServiceBusTriggerDemo/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-import azure.functions as func
-
-def main(message: func.ServiceBusMessage):
-
-    value = message.get_body().decode('utf-8')
-    logging.info('Message: %s', value)
